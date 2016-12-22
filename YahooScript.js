3b09965e125ebbd03d7be0a2942a2caaa13a6ff0
sreekanthreddy1712 <sree1712@gmail.com>
2016-12-22 13:38:21 -0600

    Add files via upload
    added files to initial project

---
+++ YahooScript.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $(".product-title").click(function () {
+        $(".product-issues").slideUp(500);
+        $(this).next().slideDown(500);
+        $(".product-title >.outer-circle-green").addClass("outer-circle-orange");
+        $(this).children().eq(0).removeClass('outer-circle-orange');
+    });
+});
